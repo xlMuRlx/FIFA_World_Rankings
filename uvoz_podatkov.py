94093b6953e7f062467a94b334bd97ae5e3db432
xlMuRlx <andraz.mur44@gmail.com>
2018-11-02 18:14:16 +0100

    Popravljanje napak

--- uvoz_podatkov.py
+++ uvoz_podatkov.py
@@ -9,6 +9,7 @@ import re
 
 orodja.pripravi_imenik("spletna.csv")
 
+
 for i in range(1, 9):
     url = (
         "https://www.imdb.com/search/title?num_votes=2000,&sort=user_rating,"
@@ -16,9 +17,11 @@ for i in range(1, 9):
     ).format(i)
     orodja.shrani_spletno_stran(url, "serije-{}.html".format(i))
 
+
 besedilo = ""
 for i in range(1, 9):
     besedilo = besedilo + "\n" + orodja.vsebina_datoteke("serije-{}.html".format(i))
+
 
 
 ###############################################################################
@@ -38,35 +41,82 @@ def poisci_epizode(niz):
         seznam.append(ujemanje.group(1))
     return seznam
 
+
 vzorec_epizode = re.compile(
     r"<span class=\"lister-item-index unbold text-primary\">"
-    r"\d\.</span>.*?<a href=.*?>(?P<serija>.*?)</a>.*?"
-    r"</small>.*?<a href=.*?>(?P<epizoda>.*?)</a>.*?"
-#    r"<span class=\"lister-item-year text-muted unbold\">\((?P<leto>\d{4})\)</span.>.*?"
-    r"<span class=\"runtime\">(?P<dolzina>.*?) min</span>.*?"
-    r"<span class=\"genre\">(?P<zvrst>.*?)</span>.*?"
-    r"imdb-rating\"></span>.*?<strong>(?P<ocena>.*?)</strong>.*?"
-    r"Director:.*?>(?P<reziser>.*?)</a>.*?"
-    r"<span name=\"nv\" data-value=\"\d+?\">(?P<st_glasov>.*?)</span>.*?",
+    r"\d+\.</span>.*?<a href=.*?>(?P<serija>.+?)</a>.*?"
+    r"</small>.*?<a href=.*?>(?P<epizoda>.+?)</a>.*?"
+    r"<span class=\"lister-item-year text-muted unbold\">\(\D*?\s?(?P<leto>\d+)\)</span>.*?"
+    r"(<span class=\"runtime\">?(?P<dolzina>.*?) min</span>?.*?)?"
+    r"<span class=\"genre\">(?P<zanr>.+?)</span>.*?"
+    r"imdb-rating\"></span>.*?<strong>(?P<ocena>.+?)</strong>.*?"
+    r"Directors?:(?P<reziserji>.+?)<span class=\"ghost\">.*?"
+    r"<span name=\"nv\" data-value=\"\d+?\">(?P<st_glasov>.+?)</span>.*?",
     re.DOTALL
 )
+
+
+vzorec_osebe = re.compile(
+    r"<a\s+href=\"/name/nm(?P<id>\d+)/?[^>]*?>(?P<ime>.+?)</a>",
+    re.DOTALL
+)
+
+
+def izloci_osebe(niz):
+    osebe = []
+    for oseba in vzorec_osebe.finditer(niz):
+        osebe.append({
+            'id': int(oseba.groupdict()['id']),
+            'ime': oseba.groupdict()['ime'],
+        })
+    return osebe
+
+
+def izloci_zanre(epizode):
+    zanri = []
+    for element in epizode:
+        for zanr in element.pop('zanr'):
+            zanri.append({'serija': element['serija'], 'zanr': zanr})
+    zanri.sort(key=lambda zanr: (zanr['serija'], zanr['zanr']))
+    return zanri
+
 
 def pocisti_podatke(podatki):
     podatki['serija'] = podatki['serija'].strip()
     podatki['epizoda'] = podatki['epizoda'].strip()
-#    podatki['leto'] = int(podatki['leto'])
-    podatki['dolzina'] = int(podatki['dolzina'])
-    podatki['zvrst'] = podatki['zvrst'].strip()
-    podatki['zvrst'] = podatki['zvrst'].split(',')
+    podatki['leto'] = int(podatki['leto'])
+    ## Problem nastopi pri prvi razbrani epizodi; ker gre za komično serijo, 
+    ## nastavimo njeno dolžino na 20 min
+    if podatki['dolzina']:
+        podatki['dolzina'] = int(podatki['dolzina'])
+    else:
+        podatki['dolzina'] = 20
+    podatki['zanr'] = podatki['zanr'].strip().split(', ')
     podatki['ocena'] = float(podatki['ocena'])
-    podatki['reziser'] = podatki['reziser'].strip()
-    podatki['st_glasov'] = float(podatki['st_glasov'].replace(',', '.'))
+    podatki['reziserji'] = izloci_osebe(podatki['reziserji'])
+    podatki['st_glasov'] = int(podatki['st_glasov'].replace(',', ''))
     return podatki
 
+
 zapis_serij = []
-for ujemanje in vzorec_epizode.finditer(besedilo):
-    podatki_epizode = pocisti_podatke(ujemanje.groupdict())
-    zapis_serij.append(podatki_epizode)
+razbitje = poisci_epizode(besedilo)
+for i in range(0, len(razbitje)):
+    for ujemanje in vzorec_epizode.finditer(razbitje[i]):
+        podatki_epizode = pocisti_podatke(ujemanje.groupdict())
+        zapis_serij.append(podatki_epizode)
+
+zanri = izloci_zanre(zapis_serij)
+
+
+# Znebimo se nepotrebnih ponovitev zapisa žanrov posamezne serije
+
+posodobi_zanre = []
+for element in zanri:
+    if element not in posodobi_zanre:
+        posodobi_zanre.append(element)
+
+zanri = posodobi_zanre
+
 
 
 ###############################################################################
@@ -75,5 +125,6 @@ for ujemanje in vzorec_epizode.finditer(besedilo):
 
 orodja.zapisi_json(zapis_serij, 'vse_epizode.json')
 
-orodja.zapisi_csv(zapis_serij, ["serija", "epizoda", "leto", "dolzina",
-    "zvrst", "ocena", "reziser", "st_glasov"], 'vse_epizode.csv')
+orodja.zapisi_csv(zapis_serij, ["serija", "epizoda", "leto", "dolzina", "reziserji",
+    "ocena", "st_glasov"], 'vse_epizode.csv')
+orodja.zapisi_csv(zanri, ['serija', 'zanr'], 'zanri.csv')
